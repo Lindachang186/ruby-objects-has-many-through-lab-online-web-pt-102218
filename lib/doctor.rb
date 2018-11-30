460f042def346b17a8a9f0e7eef9f4b957139619
Linda Chang <lindachang186@gmail.com>
2018-11-30 19:37:29 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+require "pry"
+
+class Doctor
+
+  attr_accessor :name
+
+  @@all_doctors = []
+
+  def initialize(name)
+    @name = name
+    @@all_doctors << self
+  end
+
+  def self.all
+    @@all_doctors
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+
+
+
+end
