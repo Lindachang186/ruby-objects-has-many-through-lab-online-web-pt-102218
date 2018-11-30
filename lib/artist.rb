a9435bc4b18eecbab536fc8fad55f5002b601e47
Linda Chang <lindachang186@gmail.com>
2018-11-30 17:02:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,29 @@
+require "pry"
+
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :genre, :song
 
   @@all = []
 
   def initialize(name)
     @name = name
+    @genre = genre
     @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(name, genre)
+  end
+
+  def songs
+    Songs.all.select do |song|
+      song.artist == self
+    end
   end
 
 
